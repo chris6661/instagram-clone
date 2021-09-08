fb48f3ae308ab022b14097521fd8c0356f7d123f
chris6661 <74381468+chris6661@users.noreply.github.com>
2021-09-07 23:24:31 -0500

    added seed file, added lib file, added context file

---
+++ insta-clone/src/lib/firebase.js
@@ -0,0 +1,15 @@
+import Firebase from 'firebase/app'; 
+import 'firebase/firestore'; 
+import 'firebase/auth'; 
+
+//here import the seed file
+
+const config = {}; 
+
+const firebase = Firebase.initializeApp(config); 
+const { FieldValue } = Firebase.firestore; 
+
+//call seed file only ONCE
+//seedDatabase(firebase); 
+
+export { firebase, FieldValue }; 
